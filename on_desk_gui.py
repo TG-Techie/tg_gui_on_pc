c283b051472cb3468b5d3b4a5df1ff60a408c48e
TG-Techie <TGTechie01@gmail.com>
2019-04-06 19:48:47 -0400

    Signed-off-by: TG-Techie <TGTechie01@gmail.com>

--- on_desk_gui.py
+++ on_desk_gui.py
@@ -1,16 +1,16 @@
 from tg_gui import gui, system, system_handler
-import pygame
+import pygame, time
 from math import pi
 from pygame.locals import *
 
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
-print(dir(screen))
+pygame.font.init()
 
 def color(r,g,b):
     return (r,g,b)
-
+5
 def round_rect(x, y, width, height, radius, color):
     pygame.draw.rect(screen, color, (x+radius ,y,width-radius*2,height), 0)
     pygame.draw.rect(screen, color, (x ,y+radius,width,height-radius*2), 0)
@@ -22,15 +22,22 @@ def round_rect(x, y, width, height, radius, color):
 def text(*args, **kwargs):
     return
 
+def place_text(x, y, text, color = (255,255,255), background = (0,0,0),size = 1):
+    for line in text.split('\n'):
+        myfont = pygame.font.SysFont('Sans Serif', int(20*size))
+        textsurface = myfont.render(line, False, color)
+        screen.blit(textsurface,(x,y))
+        y+= size*20
+    
 #setup gui
 gui.color_max = 255
 gui.color = color
 gui.good_text_size = 1
 gui.good_gap_size = 10
 gui.good_widget_size = 100
-gui.aprox_char_size = [7,10]
+gui.aprox_char_size = [5,20]
 gui.round_rect = round_rect
-gui.place_text = text
+gui.place_text = place_text
 
 #setup system
 system.x = 0
@@ -38,12 +45,60 @@ system.y = 0
 system.width = 800
 system.height = 600
 
+#pygame.display.update()
+#time.sleep(4000)
+
+
 system.init()
 
-pygame.display.update()
+should_loop = True
+twas_pointed = False
+pointer = 1
+button = 2
+mode = pointer
+while should_loop:
+    #system.repl_query()
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            mode = button
+            key =  event.key 
+            if key == K_ESCAPE:
+                running = False
+            elif key == K_RETURN:
+                system_handler.push_event('mv.prs')
+            elif key == K_d or key==K_RIGHT:
+                system_handler.push_event('mv.n')
+            elif key == K_a or key == K_LEFT:
+                system_handler.push_event('mv.p')
+            elif key == K_h:
+                system_handler.push_event('ld.h')
+            elif key == K_s:
+                system_handler.push_event('ld.s')
+        elif event.type == QUIT:
+            should_loop = False
+    pointer_state = pygame.mouse.get_pressed()
+    pointer_pos = pygame.mouse.get_pos()
+
+    if pointer_state[0]:
+        system_handler.push_event('ptr.g.'+str(pointer_pos).replace(' ',''),'ptr.d')
+        twas_pointed = True
+        mode = pointer
+             
+    else:
+        if twas_pointed:
+            system_handler.push_event('ptr.prs')
+            twas_pointed = False
+    if mode == pointer:
+        system_handler.push_event('ptr.u')
+    elif mode == button:
+        system_handler.push_event('mv')
+
     
-while True:
-    system.repl_query()
-    print(system.cycle())
-    
+    ret = system.cycle()
+    #if len(ret):
+        #print(ret)
     pygame.display.update()
+
+    if not should_loop:
+        pygame.quit()
+        break
