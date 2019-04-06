f8ee3a8ce7b0367ac176b1060895df57f47ecb59
TG-Techie <TGTechie01@gmail.com>
2019-04-05 23:12:49 -0400

    updates

--- on_desk_gui.py
+++ on_desk_gui.py
@@ -6,24 +6,12 @@ from pygame.locals import *
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
+print(dir(screen))
 
 def color(r,g,b):
     return (r,g,b)
 
-'''cur_surfs = []
-def place_rect(x,y,width, height, color):
-    surf = pygame.Surface((width, height))
-    surf.fill(color)
-    rect = surf.get_rect()
-    screen.blit(surf, (x, y))
-    
-
-place_rect(0,0,30,50, color(255,128,96))
-
-pygame.display.flip()
-'''
-
-def rounded_rect(x, y, width, height, radius, color):
+def round_rect(x, y, width, height, radius, color):
     pygame.draw.rect(screen, color, (x+radius ,y,width-radius*2,height), 0)
     pygame.draw.rect(screen, color, (x ,y+radius,width,height-radius*2), 0)
     pygame.draw.circle(screen, color, (x+radius,y+radius), radius, 0)
@@ -34,9 +22,28 @@ def rounded_rect(x, y, width, height, radius, color):
 def text(*args, **kwargs):
     return
 
+#setup gui
+gui.color_max = 255
+gui.color = color
+gui.good_text_size = 1
+gui.good_gap_size = 10
+gui.good_widget_size = 100
+gui.aprox_char_size = [7,10]
+gui.round_rect = round_rect
+gui.place_text = text
 
-print(dir(system))
+#setup system
+system.x = 0
+system.y = 0
+system.width = 800
+system.height = 600
+
+system.init()
+
+pygame.display.update()
     
-while 1:
-    system.query_repl()
+while True:
+    system.repl_query()
+    print(system.cycle())
+    
     pygame.display.update()
